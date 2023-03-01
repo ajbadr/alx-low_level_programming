c46c86ea11adbf312592238bf111955bf107c58c
AJ badr <polarise.a00@gmail.com>
2023-03-01 03:29:44 -0800

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
 *main - initial hello world C
-*main output text
-*return: value of 'main' is 0
+*Description: 'the program's description
+*Return: Always 0 (Success)
 */
 int main(void)
 {
