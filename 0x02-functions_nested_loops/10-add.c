a84d2ccce582ef2647dc45790ecec08b166ecc4e
AJ badr <polarise.a00@gmail.com>
2023-03-04 16:32:42 -0800

    c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -5,6 +5,7 @@
 	 * add - adds two integers and returns the result
 	 * @a: int to be added to b
 	 * @b: int to be added to a
+* Description: the program's description
 	 * Return: sum of a and b
 	 */
 	int add(int a, int b)
