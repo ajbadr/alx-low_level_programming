e41a8054a34fc53c7b54efc564740d1b8881a5f7
AJ badr <polarise.a00@gmail.com>
2023-03-04 16:24:09 -0800

    c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,7 +4,7 @@
 	/**
 	 * jack_bauer - prints every minute of the day of Jack Bauer
 	 * starting from 00:00 to 23:59
-	 * Description:'the program's description'
+	 * description:'the program's description'
 	 */
 	void jack_bauer(void)
 	{
