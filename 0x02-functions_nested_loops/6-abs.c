73c6799c4a4dead6d70e0ecd17780b5fd5fe8f32
AJ badr <polarise.a00@gmail.com>
2023-03-04 16:11:44 -0800

    c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,6 +4,7 @@
 	/**
 	 * _abs - computes the absolute value of an integer
 	 * @n: the int to check
+* Description: 'the program's description'
 	 * Return: the absolute value of int
 	 */
 	int _abs(int n)
